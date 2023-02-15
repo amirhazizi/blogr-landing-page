da34e2eb28ed0d1e690820126182cf39a7c42b1f
amir <amirazizihosein@gmail.com>
2023-02-15 10:23:55 +0330

    update js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const navMenuBtn = document.querySelector(".nav-menu")
 const navCloseBtn = document.querySelector(".nav-close")
 const smallMenu = document.querySelector(".nav-fixed-menu")
 const smallMenuBtns = document.querySelectorAll(".small-nav-btn")
+const bignavBtns = document.querySelectorAll(".big-nav-btn")
+
 navMenuBtn.addEventListener("click", () => {
   smallMenu.classList.add("active")
   navMenuBtn.classList.add("unshow")
@@ -12,13 +14,30 @@ navCloseBtn.addEventListener("click", () => {
   navMenuBtn.classList.remove("unshow")
   navCloseBtn.classList.add("unshow")
 })
+
 smallMenuBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.parentElement.nextElementSibling.classList.toggle("active-nav")
     btn.nextElementSibling.classList.toggle("rotate-arrow")
+    btn.classList.toggle("active-btn")
     smallMenuBtns.forEach((b) => {
       if (b !== btn) {
         b.parentElement.nextElementSibling.classList.remove("active-nav")
+        b.nextElementSibling.classList.remove("rotate-arrow")
+      }
+    })
+  })
+})
+bignavBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(btn.parentElement.nextElementSibling)
+    btn.parentElement.nextElementSibling.classList.toggle("active-big-nav")
+    btn.nextElementSibling.classList.toggle("rotate-arrow")
+    btn.classList.toggle("active-big-nav-btn")
+    bignavBtns.forEach((b) => {
+      if (b !== btn) {
+        b.parentElement.nextElementSibling.classList.remove("active-big-nav")
+        b.nextElementSibling.classList.remove("rotate-arrow")
       }
     })
   })
